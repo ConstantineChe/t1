4558da0abefe376d4e3465cb33178c6dacb0b564
Kostiantyn Cherkashyn <che.constantine@gmail.com>
2020-07-07 18:21:12 +0300

    done

---
+++ src/t1/core.clj
@@ -0,0 +1,95 @@
+(ns t1.core)
+
+(declare evaluate)
+
+(defn- resolve-args [vars op args]
+  (let [args-count (count args)]
+    (when (= 0 args-count)
+      (throw (new Exception
+                  (format "Parse error: 0 arguments appliued to %s function" (name op)))))
+    (map (fn [x]
+           (cond
+             (and (= op 'abs) (not= 1 args-count))
+             (throw (new Exception
+                         (format "Parse error: abs takes 1 argument %d given" args-count)))
+
+             (and (= op 'power) (not= 2 args-count))
+             (throw (new Exception
+                         (format "Parse error: power takes 2 arguments %d given" args-count)))
+
+             (number? x) x
+
+             (symbol? x) ((keyword x) vars)
+
+             (list? x) (evaluate vars x)
+             :else (throw (new Exception (format "Parse error: %d is not a number" x)))))
+         args)))
+
+(defn evaluate [vars [op & args]]
+  (let [args (resolve-args vars op args)]
+    (cond
+      (#{'+ '- '* '/} op) (apply (resolve op) args)
+
+      (= 'abs op) (Math/abs (first args))
+
+      (= 'power op) (Math/pow (first args) (second args))
+
+      :else (throw (new Exception (format "Parse error: cannot resolve operation %s" op))))))
+
+(defn- optimize* [op arg expr]
+  (cond
+    (not arg) expr
+
+    (symbol? arg) (concat expr [arg])
+
+    (seq? arg) (concat expr [arg])
+
+    (#{'- '+} op)
+    (if (zero? arg) expr (concat expr [arg]))
+
+    (= op '*) (if (zero? arg)
+                (reduced 0)
+                (if (= 1 arg) expr (concat expr [arg])))
+
+    (= op '/) (if (= 1 arg) expr (concat expr [arg]))
+    :else (concat expr [arg])))
+
+(defn optimize [[op & args]]
+  (let [expr (reduce (fn [expr arg]
+                       (if (list? arg)
+                         (let [optimized (optimize arg)]
+                           (optimize* op optimized expr))
+                         (optimize* op arg expr)))
+                     (list op) args)]
+    (cond
+      (number? expr) expr
+      (symbol? expr) expr
+
+      (= 1 (count expr)) 0
+
+      (and (#{'- '+ '/ '*} op) (= 2 (count expr)))
+      (second expr)
+
+      :else expr)))
+
+(defn- get-vars [[op & args]]
+  (reduce (fn [vars arg]
+            (if (symbol? arg)
+              (conj vars arg)
+              (if (list? arg)
+                (concat vars (get-vars arg))
+                vars))) [] args))
+
+(defn- ->javascript* [[op & args]]
+  (str "("
+       (apply str (interpose (str " " (name op) " ") ;don't want extra points for power and asb here
+                             (map (fn [arg]
+                                    (if (list? arg)
+                                      (->javascript* arg)
+                                      arg))
+                                  args)))
+       ")"))
+
+(defn ->javascript [name expr]
+  (str "function " name "(" (apply str (interpose ", " (get-vars expr)))
+       ") { return " (->javascript* expr) "; }"))
---
+++ test/t1/core_test.clj
@@ -0,0 +1,42 @@
+(ns t1.core-test
+  (:require [clojure.test :refer :all]
+            [t1.core :refer :all]))
+
+(deftest evaluate-test
+  (testing "operations."
+    (is (= (evaluate {} '(+ 1 1)) 2))
+    (is (= (evaluate {} '(- 2 1)) 1))
+    (is (= (evaluate {} '(* 2 2)) 4))
+    (is (= (evaluate {} '(/ 6 3)) 2))
+    (is (= (evaluate {} '(abs -3)) 3))
+    (is (= (evaluate {} '(power 2 4)) 16.0)))
+
+  (testing "variables resolution"
+    (is (= (evaluate {:x 10} '(* x x)) 100))
+    (is (= (evaluate {:x 2 :y 3} '(power x (* x y))) 64.0)))
+
+  (testing "Nested operations"
+    (is (= (evaluate {} '(/ 6 (+ 1 2))) 2))
+    (is (= (evaluate {} '(power (+ 2 2) (abs (- 4 5)))) 4.0)))
+
+  (testing "Exceptions"
+    (is (thrown? Exception (evaluate {} '(- 1 (+) 2))))
+    (is (thrown? Exception (evaluate {} '(pow "1" 2))))
+    (is (thrown? Exception (evaluate {} '(power 1 2 3))))
+    (is (thrown? Exception (evaluate {} '(pow 1 2))))
+    (is (thrown? Exception (evaluate {} '(abs 1 2 3))))))
+
+(deftest optimize-test
+  (testing "addition"
+    (is (= (optimize '(+ 1 0 2)) '(+ 1 2)))
+    (is (= (optimize '(+ x 0)) 'x))
+    (is (= (optimize '(* x (+ 2 0))) '(* x 2))))
+  (testing "multiplication"
+    (is (= (optimize '(* x 1)) 'x))
+    (is (= (optimize '(* x (+ 1 (/ y 1)))) '(* x (+ 1 y))))
+    (is (= (optimize '(* x (+ 1 (* y 0 1 2 (+ 4 3) 4 5)))) 'x))))
+
+(deftest ->javascript-test
+  (testing "->javascript"
+    (is (= (->javascript "example" '(+ 1 x y (* 4 z) 6))
+           "function example(x, y, z) { return (1 + x + y + (4 * z) + 6); }"))))
